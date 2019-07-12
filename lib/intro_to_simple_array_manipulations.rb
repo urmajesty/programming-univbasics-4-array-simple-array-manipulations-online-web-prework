ecc04383211edd2fe7ff6422be1c0daaaad5b493
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-12 08:42:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, string)
+  array.push("violet")
+end
+
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+  return pop_with_args(array)
+end
